5b6be7a0cef6e60ce51cd1830c9a0e311f93a926
Lesnyak Nikita <lesnyak_2011@mail.ru>
2025-01-14 16:41:30 +0300

    feat(project): костыль для типов календаря

--- src/components/task/edit/date/index.tsx
+++ src/components/task/edit/date/index.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { DatePicker } from '@nextui-org/react';
+import { DatePicker, DateValue } from '@nextui-org/react';
 import { useFormContext } from 'react-hook-form';
-import { parseDate, now, getLocalTimeZone, CalendarDate } from '@internationalized/date';
+import { parseDate, now, getLocalTimeZone } from '@internationalized/date';
 import { getCurrentGlobalDate } from '@utils/common';
 import { getLocalISOString } from '@utils/date';
 
@@ -21,9 +21,15 @@ const DateComponent: React.FC<DateI> = ({ dateTill, isLoading }) => {
   register(fieldId);
 
   const endDate = useMemo(() => getCurrentGlobalDate(dateTill), [dateTill]);
+  const defaultValue = endDate ? (parseDate(endDate) as unknown as DateValue) : null;
+  const minValue = now(getLocalTimeZone()) as unknown as DateValue;
 
   const onChange = useCallback(
-    (calendarDate: CalendarDate) => {
+    (calendarDate: DateValue | null) => {
+      if (!calendarDate) {
+        return;
+      }
+
       const { day, month, year } = calendarDate;
       const date = new Date(year, month - 1, day);
       const localISO = getLocalISOString(date);
@@ -38,8 +44,8 @@ const DateComponent: React.FC<DateI> = ({ dateTill, isLoading }) => {
       <DatePicker
         isDisabled={isLoading}
         label={t('input.date.label')}
-        defaultValue={endDate ? parseDate(endDate) : null}
-        minValue={now(getLocalTimeZone())}
+        defaultValue={defaultValue}
+        minValue={minValue}
         onChange={onChange}
         isInvalid={false}
         popoverProps={{
